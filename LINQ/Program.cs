7e237ccd288e0b867fe6d4f32ce694b0cb4e9899
bchollid <brandonholliday1@gmail.com>
2024-01-11 12:07:22 -0700

    Finished

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() {"Last Of Us", "Cult of the Lamb", "Baldur's Gate 3", "Age of Empires 4", "World of Warcraft", "The Finals" };
+            var orderedByLength = videoGames.OrderBy(nameOfGame => nameOfGame.Length);
+            foreach(var game in orderedByLength)
+            {
+                Console.WriteLine(game);
+            }
+            Console.ReadLine();
         }
     }
 }
